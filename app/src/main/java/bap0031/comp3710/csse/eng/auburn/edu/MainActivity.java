8e01ae2a83ff49303d0dc33ced2f3911b3097b6e
Bradley <bradleyprobert@sw.rr.com>
2017-04-07 21:22:23 -0500

    initial commit

---
+++ app/src/main/java/bap0031/comp3710/csse/eng/auburn/edu/MainActivity.java
@@ -0,0 +1,13 @@
+package bap0031.comp3710.csse.eng.auburn.edu;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
