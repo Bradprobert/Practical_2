c15bc8710327255672bc4a5445ae8e75bcafd491
Bradley <bradleyprobert@sw.rr.com>
2017-04-07 22:57:04 -0500

    game grid initializes correctly with testing

--- app/src/main/java/bap0031/comp3710/csse/eng/auburn/edu/GameGrid.java
+++ app/src/main/java/bap0031/comp3710/csse/eng/auburn/edu/GameGrid.java
@@ -1,5 +1,7 @@
 package bap0031.comp3710.csse.eng.auburn.edu;
 
+import java.util.Random;
+
 /**
  * Created by bradley on 4/7/17
  */
@@ -10,7 +12,27 @@ public class GameGrid {
 
     public GameGrid() {
         grid = new int[4][4];
+        initializeGrid();
     }
 
+    private void initializeGrid() {
+        Random random = new Random();
 
+        for (int i = 0; i < 2; i++) {
+            int row = random.nextInt(4);
+            int column = random.nextInt(4);
+
+            int startValue;
+            if (random.nextInt(100) < 75)
+                startValue = 2;
+            else
+                startValue = 4;
+
+            grid[row][column] = startValue;
+        }
+    }
+
+    public int[][] getGrid() {
+        return grid;
+    }
 }
