de73f175dd0a38b8953e66d7307f5670b87d11a0
Bradley <bradleyprobert@sw.rr.com>
2017-04-17 10:19:23 -0500

    push all files and changes. nothing specific

--- app/src/main/java/bap0031/comp3710/csse/eng/auburn/edu/controller/Grid2048Controller.java
+++ app/src/main/java/bap0031/comp3710/csse/eng/auburn/edu/controller/Grid2048Controller.java
@@ -10,5 +10,10 @@ import bap0031.comp3710.csse.eng.auburn.edu.model.Grid2048;
 
 public class Grid2048Controller {
     private GridLayout gridLayout;
+
+    public Grid2048Controller(GridLayout gridLayout) {
+        this.gridLayout = gridLayout;
+    }
+
     private Grid2048 grid;
 }
